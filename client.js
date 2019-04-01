cc4972592fb7b309e9baf5a6ed390d9db10bde9d
Xyvyrianeth <strobinsson@gmail.com>
2019-04-01 11:36:25 -0500

    Update client.js

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.login(process.env.TOKEN).then(() => {
     client.on("ready", () => {
         client.guilds.get("399327996076621825").channels.get("562313696420823042").send("Version " + respond.version + " published successfully!");
-    })
+    });
 }).catch(err => {
     console.log("Error logging in:\n" + err);
 });
