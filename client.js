c0f7515272607a0744b03e3667181d1cc73e9a1d
Xyvyrianeth <strobinsson@gmail.com>
2019-04-01 11:41:54 -0500

    Update client.js

--- client.js
+++ client.js
@@ -5,10 +5,10 @@ client.login(process.env.TOKEN).then(() => {}).catch(err => {
     console.log("Error logging in:\n" + err);
 });
 client.on("ready", () => {
-    client.guilds.get("399327996076621825").channels.get("562313696420823042").send("Version " + respond.version + " published successfully!");
+    client.guilds.get("399327996076621825").channels.get("562313696420823042").send("Version " + version + " published successfully!");
 });
 
-var { respond } = require("./respond.js");
+var { respond, version } = require("./respond.js");
 
 client.on('message', message => {
     respond(message);
