ff15fb042f9a5c5d2792091a4e7d3a2ede139a71
Xyvyrianeth <strobinsson@gmail.com>
2019-04-01 11:39:48 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login(process.env.TOKEN).then(() => {
-    client.on("ready", () => {
-        client.guilds.get("399327996076621825").channels.get("562313696420823042").send("Version " + respond.version + " published successfully!");
-    });
-}).catch(err => {
+client.login(process.env.TOKEN).then(() => {}).catch(err => {
     console.log("Error logging in:\n" + err);
+});
+client.on("ready", () => {
+    client.guilds.get("399327996076621825").channels.get("562313696420823042").send("Version " + respond.version + " published successfully!");
 });
 
 var { respond } = require("./respond.js");
